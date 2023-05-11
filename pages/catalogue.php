d4c73099dbb6cf000a01e4efca3c2bd6ed4f7c53
Gaël COUGNARD <gael.cougnard@edu.ece.fr>
2023-05-11 18:01:27 +0200

    ajout des nouvelles pages

---
+++ pages/catalogue.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>catalogue</title>
+    <link rel="stylesheet" href="catalogue.css">
+</head>
+<body>
+
+    ici faut mettre le header
+
+
+    <h1 class="produits_phares">
+        NOS PRODUITS PHARES
+    </h1>
+
+    <div class="slider">
+
+    </div>
+
+
+    <div id="catalogue">
+        <div class="filtre">
+
+        </div>
+
+        <div class="categorie">
+            
+        </div>
+
+        <div class="produits">
+            <div class="produit_1"></div>
+            <div class="produit_2"></div>
+            <div class="produit_3"></div>
+        </div>
+    </div>
+
+
+
+</body>
+</html>
