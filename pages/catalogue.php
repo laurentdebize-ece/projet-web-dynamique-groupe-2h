0172d464648182f50bd2ed924e27b66321217d9c
Gaël COUGNARD <gael.cougnard@edu.ece.fr>
2023-05-11 20:40:57 +0200

    catalogue mise a jour avancée

--- pages/catalogue.php
+++ pages/catalogue.php
@@ -20,14 +20,33 @@
 
 
     <div id="catalogue">
-        <div class="filtre">
 
+        <div class="filtre_categorie">
+            <div class="filtre">
+                <h3 id="titre_filtre">
+                    FILTRES
+                </h3>
+                <label><input type="checkbox" name="prix_croissant" value="prix_croissant">Prix croissant</label> <br>
+                <label><input type="checkbox" name="prix_decroissant" value="prix_decroissant">Prix décroissant</label> <br>
+                <label><input type="checkbox" name="recent" value="recent">Récent</label> <br>
+                <label><input type="checkbox" name="ancien" value="ancien">Ancien</label> <br>
+                <h4 id="appliquer_filtre">
+                    Appliquer
+                </h4>
+            </div>
+
+            <div class="categorie">
+                <h3 id="titre_categories">
+                    CATEGORIES
+                </h3>
+                <ul>
+                    <li>Interieur</li>
+                    <li>Exterieur</li>
+                    <li>Sensation</li>
+                    <li>En famille</li>
+                </ul>
+            </div>
         </div>
-
-        <div class="categorie">
-            
-        </div>
-
         <div class="produits">
             <div class="produit_1"></div>
             <div class="produit_2"></div>
