091d7bd8db8034a328bcae42f0f142b5d7bba591
REINERT-Aurelien <92569325+REINERT-Aurelien@users.noreply.github.com>
2023-05-14 14:17:21 +0200

    Add files via upload
    footer, menu

---
+++ pages/menu.php
@@ -0,0 +1,16 @@
+<header>
+    <nav class="navbar">
+        <a href="#" class="logo">Gifty</a>
+        <ul class="nav-links">
+            <li><a href="" class="active">Accueil</a></li>
+            <li><a href="">J'ai une OmnesBox</a></li>
+            <li><a href="">Partenaires</a></li>
+            <li><a href="">Catalogue</a></li>
+        </ul>
+        <ul class="nav-icons">
+            <li><img src="../assets/search.svg" alt="Rechercher"></li>
+            <li><img src="../assets/account.svg" alt="Compte"></li>
+            <li><img src="../assets/cart.svg" alt="Panier"></li>
+        </ul>
+    </nav>
+</header>
