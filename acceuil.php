9c71551bfd36099d5486248a43c3d246edb26007
guillaume <guillaume.patoz@edu.ece.fr>
2023-05-15 14:09:01 +0200

    Merge main into guillaume

---
+++ acceuil.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>form</title>
+</head>
+<body>
+    <nav>
+        <div>
+            
+        </div>
+    </nav>
+</body>
+</html>   
