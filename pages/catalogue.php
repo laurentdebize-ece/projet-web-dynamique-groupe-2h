7d1e9aecde177e3efb32aee0153a1033336cdbc6
Gaël COUGNARD <gael.cougnard@edu.ece.fr>
2023-05-17 16:38:46 +0200

    .

--- pages/catalogue.php
+++ pages/catalogue.php
@@ -6,6 +6,9 @@
     <link rel="stylesheet" href="catalogue.css">
     <link rel="stylesheet" href="main/main.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 </head>
 <body>
 <?php include('main/menu.php') ?>
