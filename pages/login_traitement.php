b308fcfb23659c09632de984aa84801be68261db
Louis <louis.duvanel@edu.ece.fr>
2023-05-17 13:20:53 +0200

    .

--- pages/login_traitement.php
+++ pages/login_traitement.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,53 +9,86 @@
     <link rel="stylesheet" href="../stylesheet/login.css">
     <title>Document</title>
 </head>
+
 <body>
     <?php
     $verification = 0;
-    if(isset($_POST['identifiant']) && isset($_POST['mdp'])){
+    if (isset($_POST['identifiant']) && isset($_POST['mdp'])) {
 
         include("connexion_base.php");
 
-        $reponse = $bdd->prepare("SELECT email, mot_de_passe FROM Users WHERE email= ? AND mot_de_passe= ?");
-        $reponse->execute(array($_POST['identifiant'], $_POST['mdp']));
+        //Test pour savoir si c'est un partenaire qui veut changer son mot de passe 
+        if (substr($_POST['mdp'], -4) === "0000") {
 
-        while ($donnees = $reponse->fetch()) {
-            $verification++;
+            //Un partenaire va changer son mot de passe
+    
+            $reponse3 = $bdd->query("SELECT * FROM Users");
+            $statut;
+            //On verifie que c'est bien un partenaire 
+            while ($donnees = $reponse3->fetch()) {
+                if ($_POST['identifiant'] === $donnees['email']) {
+                    //Alors on a bien un partenaire correspondant
+                    $statut = $donnees['statut'];
+                    if ($statut != 2) {
 
-            if(htmlspecialchars($_POST['identifiant'])===$donnees['email'] && htmlspecialchars($_POST['mdp'])===$donnees['mot_de_passe']){
-                session_start(); 
-                $_SESSION['identifiant'] = $_POST['identifiant'];
-                $_SESSION['mdp'] = $_POST['mdp'];
+            
+                        //Redirection du client à l'accueil ou de l'admin à l'accueil
+                        echo "<script>window.location.assign('http://localhost:8888/projet-web-dynamique-groupe-2h/pages/accueil.php');</script>";
 
-                //fonction pour retrouver le nom et le prenom
-
-                $reponse2 = $bdd->query("SELECT * FROM Users");
-
-                while ($donnees = $reponse2->fetch()) {
-                    if($_SESSION['identifiant']===$donnees['email'] && $_SESSION['mdp']===$donnees['mot_de_passe'] ){
-                        $_SESSION['prenom']=$donnees['prenom'];
-                        $_SESSION['nom']=$donnees['nom'];
-                        $_SESSION['tel']=$donnees['phone'];
-                        $_SESSION['statut']=$donnees['statut'];
-                        break;
+                    } else {
+                        //Faire passer la chaine en methode GET pour gerer la recherche dans la base de donnée 
+                        $chaine = $_POST['identifiant'];
+                        $url = "http://localhost:8888/projet-web-dynamique-groupe-2h/pages/login_partenaire.php?chaine=" . urlencode($chaine);
+                        header("Location: " . $url);
+                        echo "<script>window.location.assign('$url');</script>";
                     }
                 }
-                 //Acces à la page suivante
-                echo "<script>window.location.assign('http://localhost:8888/projet-web-dynamique-groupe-2h/pages/accueil.php');</script>";
             }
-        }
 
-        if($verification === 0){
-            echo"<script>$(document).ready(function (){
+        } else {
+
+            $reponse = $bdd->prepare("SELECT email, mot_de_passe FROM Users WHERE email= ? AND mot_de_passe= ?");
+            $reponse->execute(array($_POST['identifiant'], $_POST['mdp']));
+
+            while ($donnees = $reponse->fetch()) {
+                $verification++;
+
+                if (htmlspecialchars($_POST['identifiant']) === $donnees['email'] && htmlspecialchars($_POST['mdp']) === $donnees['mot_de_passe']) {
+                    session_start();
+                    $_SESSION['identifiant'] = $_POST['identifiant'];
+                    $_SESSION['mdp'] = $_POST['mdp'];
+
+                    //fonction pour retrouver le nom et le prenom
+    
+                    $reponse2 = $bdd->query("SELECT * FROM Users");
+
+                    while ($donnees = $reponse2->fetch()) {
+                        if ($_SESSION['identifiant'] === $donnees['email'] && $_SESSION['mdp'] === $donnees['mot_de_passe']) {
+                            $_SESSION['prenom'] = $donnees['prenom'];
+                            $_SESSION['nom'] = $donnees['nom'];
+                            $_SESSION['tel'] = $donnees['phone'];
+                            $_SESSION['statut'] = $donnees['statut'];
+                            break;
+                        }
+                    }
+                    //Acces à la page suivante
+                    echo "<script>window.location.assign('http://localhost:8888/projet-web-dynamique-groupe-2h/pages/accueil.php');</script>";
+                }
+            }
+
+            if ($verification === 0) {
+                echo "<script>$(document).ready(function (){
                 $('#identi').css('border', 'red solid 2px');
                 $('#mot_de_p').css('border', 'red solid 2px');
             });</script>
             <div><p class='message_erreur'>Identifiant ou mot de passe incorrectes</p></div>";
-            //COrriger ça
-        }
+                //COrriger ça
+            }
 
-        $reponse->closeCursor();
+            $reponse->closeCursor();
+        }
     }
     ?>
 </body>
+
 </html>
--- pages/settings.php
+++ pages/settings.php
@@ -17,42 +17,58 @@ session_start();
             Paramètre Admin
         </div>
         <div class="settings-second-container">
-            <div class="partenaire-container">
-                <div>
-                    <p class="legende">Ajouter un partenaire</p>
+            <div class="ajout-container">
+                <div class="partenaire-container">
+                    <div>
+                        <p class="legende">Ajouter un partenaire</p>
+                    </div>
+                    <form method="post" action="settings_traitement.php">
+                        <label for="email_partenaire"></label>
+                        <input type="email" name="email_partenaire" placeholder="email">
+                        <label for="nom_partenaire"></label>
+                        <input type="text" name="password_partenaire" placeholder="mot_de_passe">
+                        <label for="nom_partenaire"></label>
+                        <input type="text" name="nom_partenaire" placeholder="Nom">
+                        <input type="submit">
+                    </form>
                 </div>
-                <form method="post" action="settings_traitement.php">
-                    <label for="email_partenaire"></label>
-                    <input type="email" name="email_partenaire" placeholder="email">
-                    <label for="nom_partenaire"></label>
-                    <input type="text" name="password_partenaire" placeholder="mot_de_passe">
-                    <label for="nom_partenaire"></label>
-                    <input type="text" name="nom_partenaire" placeholder="Nom">
-                    <input type="submit">
-                </form>
-            </div>
-            <div class="cadeau-container">
-                <div>
-                    <p class="legende">Ajouter une carte</p>
+                <div class="cadeau-container">
+                    <div>
+                        <p class="legende">Ajouter une carte</p>
+                    </div>
+                    <form method="post">
+                        <label for="nom_carte"></label>
+                        <input type="text" name="nom_carte" placeholder="Nom Carte">
+                        <label for="prix_carte"></label>
+                        <input type="number" name="prix_carte" placeholder="Prix">
+                        <label for="description_carte"></label>
+                        <input type="text" name="description_carte" placeholder="Description">
+                        <label for="id_partenaire"></label>
+                        <input type="number" name="id_partenaire" placeholder="ID du partenaire">
+                        <input type="submit">
+                    </form>
+                    <?php
+                    include("settings_traitement_bis.php");
+                    ?>
                 </div>
-                <form method="post">
-                    <label for="nom_carte"></label>
-                    <input type="text" name="nom_carte" placeholder="Nom Carte">
-                    <label for="prix_carte"></label>
-                    <input type="number" name="prix_carte" placeholder="Prix">
-                    <label for="description_carte"></label>
-                    <input type="text" name="description_carte" placeholder="Description">
-                    <label for="id_partenaire"></label>
-                    <input type="number" name="id_partenaire" placeholder="ID du partenaire">
-                    <input type="submit">
-                </form>
-                <?php
-                include("settings_traitement_bis.php");
-                ?>
             </div>
-        </div>
-        <div class="settings-second-bis-container">
-                <p>HElllloo</p>
+            <div class="check-container">
+                <div class="dropdown">
+                    <button class="dropbtn">Activités</button>
+                    <div class="dropdown-content">
+                        <?php
+                        include("connexion_base.php");
+
+                        $reponse = $bdd->query("SELECT domaine FROM Users WHERE statut=2");
+
+                        while ($donnees = $reponse->fetch()) {
+                            $domaine = $donnees['domaine'];
+                            echo '<p>' . $domaine . '</p>';
+                        }
+                        ?>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </body>
