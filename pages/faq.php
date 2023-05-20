0114be346de441e3a64d86d8a43e6dfb52f52380
aurel <aurelien.reinert@edu.ece.fr>
2023-05-20 14:41:50 +0200

    FAQ

--- pages/faq.php
+++ pages/faq.php
@@ -13,16 +13,37 @@
         <details class="question-details">
             <summary>Qui sommes-nous ?<i class="fa fa-angle-down"></i></summary>
             <div class="description">
-                Lorem ipsum dolor sit amet consectutur.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ut blandit eros, sed interdum lectus.
+                Praesent sit amet odio tortor. Nullam felis dolor, egestas nec pulvinar in, efficitur ut dolor. Cras
+                sapien turpis, sodales eget quam et, cursus congue diam. Duis fringilla, libero quis dignissim molestie,
+                velit tortor posuere nulla, id vehicula enim quam non tellus. Duis justo risus, accumsan a ullamcorper
+                ut, gravida sed odio. Quisque erat arcu, porta a ligula a, tempor accumsan tellus. In auctor odio eu
+                purus laoreet dapibus.
             </div>
         </details>
         <details class="question-details">
             <summary>J'ai déjà une OmnesBox, comment puis-je l'utiliser ?<i class="fa fa-angle-down"></i></summary>
-            <div class="description">Lorem ipsum dolor sit amet consectutur.</div>
+            <div class="description">
+                Mauris ac libero augue. Donec dapibus sagittis orci, quis pretium turpis pharetra in. Nullam convallis
+                sed lorem vitae malesuada. Curabitur sit amet orci non orci volutpat imperdiet. Sed rutrum, nibh at
+                varius volutpat, sapien tortor eleifend quam, id mattis mauris mi et orci. Curabitur ut mauris augue.
+                Vestibulum a dolor rutrum, sodales ante ut, feugiat turpis. Quisque rhoncus finibus odio, nec iaculis
+                mauris vestibulum ut. Etiam faucibus vestibulum molestie. Nunc leo neque, molestie non lacus et, aliquam
+                gravida odio. Mauris nec enim nec turpis facilisis venenatis accumsan pellentesque libero. Maecenas
+                imperdiet vitae velit vel vulputate. Maecenas tempus scelerisque lorem, in interdum purus commodo vitae.
+                Mauris eget lobortis odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </div>
         </details>
         <details class="question-details">
             <summary>Comment utiliser une carte promotion chez un partenaire ?<i class="fa fa-angle-down"></i></summary>
-            <div class="description">Lorem ipsum dolor sit amet consectutur.</div>
+            <div class="description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ut blandit eros, sed interdum lectus.
+                Praesent sit amet odio tortor. Nullam felis dolor, egestas nec pulvinar in, efficitur ut dolor. Cras
+                sapien turpis, sodales eget quam et, cursus congue diam. Duis fringilla, libero quis dignissim molestie,
+                velit tortor posuere nulla, id vehicula enim quam non tellus. Duis justo risus, accumsan a ullamcorper
+                ut, gravida sed odio. Quisque erat arcu, porta a ligula a, tempor accumsan tellus. In auctor odio eu
+                purus laoreet dapibus.
+            </div>
         </details>
     </div>
     <?php include("footer.php") ?>
