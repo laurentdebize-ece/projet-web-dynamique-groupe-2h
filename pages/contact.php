1a05b667bb4f46643d078b97a25725d47bb5b2dd
aurel <aurelien.reinert@edu.ece.fr>
2023-05-22 18:20:07 +0200

    contact

--- pages/contact.php
+++ pages/contact.php
@@ -1,1 +1,32 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>GIFTY - Contact</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../stylesheet/main.css">
+    <link rel="stylesheet" href="../stylesheet/contact.css">
+</head>
+<body>
+    <?php include("menu.php") ?>
+    <h1 class="page-title">Nous contacter</h1>
+    <section class="main">
+        <div class="info-site">
+            <h2 class="section-title">L'entreprise</h2>
+            <p class="adresse">
+                1600 Pennsylvania Ave NW, Washington, DC 20500, États-Unis
+            </p>
+            <h2 class="section-title">Nous suivre</h2>
+        </div>
+        <div class="contact-form">
+            <h2 class="section-title">Formulaire de contact</h2>
+            <form action="" method="post">
+                <input name="name" type="text" placeholder="Nom" class="input-field">
+                <input name="email" type="email" placeholder="Adresse mail" class="input-field">
+                <textarea name="message" placeholder="Entrez ici votre message..." class="input-field"></textarea>
+                <input type="submit" value="Envoyer" class="submit-button">
+            </form>
+        </div>
+    </section>
+    <?php include("footer.php") ?>
+</body>
+</html>
--- pages/footer.php
+++ pages/footer.php
@@ -1,6 +1,6 @@
 <footer>
     <ul class="footer-elements">
-        <li><a href="">Contactez-nous</a></li>
+        <li><a href="contact.php">Contactez-nous</a></li>
         <li><a href="faq.php">FAQ</a></li>
         <li><a href="">Conditions générales de vente</a></li>
         <li><a href="">Cookies</a></li>
