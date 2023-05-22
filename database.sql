0dcf3d9d6db2b721eb2b2d497ea7ad02081e4684
aurel <aurelien.reinert@edu.ece.fr>
2023-05-22 21:33:55 +0200

    j

--- database.sql
+++ database.sql
@@ -21,27 +21,27 @@ CREATE TABLE Domaine
 
 CREATE TABLE Users
 (
-    id       int PRIMARY KEY AUTO_INCREMENT,
-    nom      varchar(20) NOT NULL,
-    prenom   varchar(20) NOT NULL,
+    id           int PRIMARY KEY AUTO_INCREMENT,
+    nom          varchar(20) NOT NULL,
+    prenom       varchar(20) NOT NULL,
     mot_de_passe varchar(32) NOT NULL,
-    email    varchar(50) NOT NULL,
-    phone    varchar(10) NOT NULL,
-    statut   int         NOT NULL,
-    domaine  int
+    email        varchar(50) NOT NULL,
+    phone        varchar(10) NOT NULL,
+    statut       int         NOT NULL,
+    domaine      int
 );
 
 
 CREATE TABLE Carte
 (
-    id          int PRIMARY KEY AUTO_INCREMENT,
-    date_ajout  date,
-    partenaire  varchar(25),
-    prix        int,
-    nomCarte    varchar(50),
+    id                int PRIMARY KEY AUTO_INCREMENT,
+    date_ajout        date,
+    partenaire        varchar(25),
+    prix              int,
+    nomCarte          varchar(50),
     description_carte varchar(200),
-    img  varchar(50),
-    mots_clefs  varchar(100)
+    img               varchar(50),
+    mots_clefs        varchar(100)
 );
 
 CREATE TABLE Achat
@@ -62,16 +62,24 @@ VALUES (NULL, 'Partenaire');
 
 INSERT INTO Users
 VALUES (NULL, 'Reinert', 'Aurélien', MD5('whleutwc'), 'aurelien.reinert@edu.ece.fr', '0670987563', 1, NULL),
-(NULL, 'Cougnard', 'Gael', MD5('123456789'), 'gael.cougnard@edu.ece.fr', '0649515524', 1, NULL),
-(NULL, 'Patoz', 'Guillaume', MD5('123456789'), 'guillaume.patoz@edu.ece.fr', '0657895112', 2, NULL);
+       (NULL, 'Cougnard', 'Gael', MD5('123456789'), 'gael.cougnard@edu.ece.fr', '0649515524', 1, NULL),
+       (NULL, 'Patoz', 'Guillaume', MD5('123456789'), 'guillaume.patoz@edu.ece.fr', '0657895112', 2, NULL);
 
 
 INSERT INTO Carte
-VALUES (NULL, '2023-05-21', 'decathlon', 25, 'football', "Profitez d'une réduction de 50% sur des cours de football auprès de nos partenaires.", "../assets/1.jpg", "exterieur, découverte"),
-(NULL, '2023/02/12', 'decathlon', 25, 'velo', "Visitez la magnifique région qu'est le Parc du Pilat en famille. Location de matériel inclus.", "../assets/veloo.jpg", "exterieur, découverte, en famille"),
-(NULL, '2022/05/10', 'Lyon métropole', 10, 'piscine', "Profitez d'une réduction de 50% sur les forfaits mensuel d'entrainement d'aquabike, d'aquadanse, d'aquagym et d'aquarunning dans une piscine municipale.", "../assets/3.jpg", "interieur, découverte"),
-(NULL, '2022/10/11', 'Val Thorens', 375, 'ski', "Profitez d'un forfait saisonnier à prix réduit !", "../assets/2.jpg", "exterieur, sensation forte"),
-(NULL, '2022/07/01', 'Mc Donald', 2, 'repas', "Profitez de 90% de réduction sur votre prochaine note.", "../assets/mcdo.jpg", "interieur");
+VALUES (NULL, '2023-05-21', 'decathlon', 25, 'football',
+        "Profitez d'une réduction de 50% sur des cours de football auprès de nos partenaires.", "../assets/1.jpg",
+        "exterieur, découverte"),
+       (NULL, '2023/02/12', 'decathlon', 25, 'velo',
+        "Visitez la magnifique région qu'est le Parc du Pilat en famille. Location de matériel inclus.",
+        "../assets/veloo.jpg", "exterieur, découverte, en famille"),
+       (NULL, '2022/05/10', 'Lyon métropole', 10, 'piscine',
+        "Profitez d'une réduction de 50% sur les forfaits mensuel d'entrainement d'aquabike, d'aquadanse, d'aquagym et d'aquarunning dans une piscine municipale.",
+        "../assets/3.jpg", "interieur, découverte"),
+       (NULL, '2022/10/11', 'Val Thorens', 375, 'ski', "Profitez d'un forfait saisonnier à prix réduit !",
+        "../assets/2.jpg", "exterieur, sensation forte"),
+       (NULL, '2022/07/01', 'Mc Donald', 2, 'repas', "Profitez de 90% de réduction sur votre prochaine note.",
+        "../assets/mcdo.jpg", "interieur");
 
 
 COMMIT;
