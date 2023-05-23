7863be4673d87f68aa16ec64329fedf5ef0852f1
guillaume <guillaume.patoz@edu.ece.fr>
2023-05-23 22:25:24 +0200

    partenaire avec bdd

---
+++ javascript/partenaire.js
@@ -0,0 +1,7 @@
+const blocSport = document.getElementById("sliderSport");
+
+function turn (name) {
+    if (name == "1") {
+        $('#sliderSport').left('calc(-(30vh + 30px + 20px)*5)');
+    }
+}
--- pages/accueil.php
+++ pages/accueil.php
@@ -12,32 +12,34 @@
 </head>
 <body>
     <?php include('menu.php') ?>
-    <h1>Accueil</h1>
-    <h2>Les actus de notre catalogue</h2>
-    <div class="actu-container">
-        <div class="arrow-button"></div>
-        <div class="img-container news">
-            <img src="../assets/1.jpg" alt="Carte football">
-            <div class="img-description">
-                <h3>Offre football</h3>
-                <p>Profitez d'une offre de réduction de 50% sur des cours de football auprès de nos partenaires.</p>
+    <div class="acceuil">
+        <h1>Accueil</h1>
+        <h2>Les actus de notre catalogue</h2>
+        <div class="actu-container">
+            <div class="arrow-button"></div>
+            <div class="img-container news">
+                <img src="../assets/1.jpg" alt="Carte football">
+                <div class="img-description">
+                    <h3>Offre football</h3>
+                    <p>Profitez d'une offre de réduction de 50% sur des cours de football auprès de nos partenaires.</p>
+                </div>
             </div>
-        </div>
-        <div class="img-container">
-            <img src="../assets/2.jpg" alt="Carte football">
-            <div class="img-description">
-                <h3>Offre football</h3>
-                <p>Profitez d'une offre de réduction de 50% sur des cours de football auprès de nos partenaires.</p>
+            <div class="img-container">
+                <img src="../assets/2.jpg" alt="Carte football">
+                <div class="img-description">
+                    <h3>Offre football</h3>
+                    <p>Profitez d'une offre de réduction de 50% sur des cours de football auprès de nos partenaires.</p>
+                </div>
             </div>
-        </div>
-        <div class="img-container news">
-            <img src="../assets/3.jpg" alt="Carte football">
-            <div class="img-description">
-                <h3>Offre football</h3>
-                <p>Profitez d'une offre de réduction de 50% sur des cours de football auprès de nos partenaires.</p>
+            <div class="img-container news">
+                <img src="../assets/3.jpg" alt="Carte football">
+                <div class="img-description">
+                    <h3>Offre football</h3>
+                    <p>Profitez d'une offre de réduction de 50% sur des cours de football auprès de nos partenaires.</p>
+                </div>
             </div>
+            <img src="../assets/next.png" class="arrow-button" alt="Suivant">
         </div>
-        <img src="../assets/next.png" class="arrow-button" alt="Suivant">
     </div>
     <?php include('footer.php') ?>
     <script src="../javascript/menu.js"></script>
